4de45b8ffcf619454976748ec719ea85da304102
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 11:12:50 +0300

    Create Main.java

---
+++ src/Task3/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// Главный класс для запуска программы
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрос у пользователя целого числа N
+        System.out.print("Введите целое число N: ");
+        int N = scanner.nextInt();
+
+        System.out.println("Последовательность Фибоначчи до " + N + ":");
+        Fibonacci.printFibonacci(N); // Вызов метода для печати последовательности
+
+        scanner.close();
+    }
+}
