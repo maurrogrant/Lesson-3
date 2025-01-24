1c1adf0ef210cffc4b6b4d85293e85b7e6325fbc
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 11:08:59 +0300

    Create Main.java

---
+++ src/Task1/Main.java
@@ -0,0 +1,13 @@
+// Главный класс для запуска программы
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        int iterations = BubbleSort.bubbleSort(array);
+        
+        System.out.println("Отсортированный массив:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nКоличество итераций: " + iterations);
+    }
+}
