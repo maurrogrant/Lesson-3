0091d5d451507b006b48410a15b990cd22d1b986
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 11:12:27 +0300

    Update and rename 3_FibonacciRecursion.java to src/Task3/Fibonacci.java

--- src/Task3/Fibonacci.java
+++ src/Task3/Fibonacci.java
@@ -1,19 +1,5 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Запрос у пользователя целого числа N
-        System.out.print("Введите целое число N: ");
-        int N = scanner.nextInt();
-
-        System.out.println("Последовательность Фибоначчи до " + N + ":");
-        printFibonacci(N);
-
-        scanner.close();
-    }
-
+// Класс для работы с последовательностью Фибоначчи
+public class Fibonacci {
     // Метод для печати последовательности Фибоначчи до N
     public static void printFibonacci(int N) {
         for (int i = 0; i < N; i++) {
